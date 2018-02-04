fbcc561c045e3afc745fcba51db075ee5ac471bc
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-02-04 22:37:40 +0200

    practice1

---
+++ practice1.py
@@ -0,0 +1,39 @@
+
+
+print("Hello World")
+
+str='I am saeed'
+print(str)
+
+Bio=("I am Saeed!\n I have completed my ICT B-Eng from Finland.\n "
+     "I am a young very energatic Man.\n I love to learn new Tech.\n I want to be a Python Guru.")
+print(Bio)
+#While loops
+Apples = 0
+while Apples <3:
+    print("Apples are not enough for the day.")
+    Apples= Apples+1
+
+name = 'Saeed'
+while name != 'your name':
+    print("Enter your name please.")
+    name = input()
+print("Thank you.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
